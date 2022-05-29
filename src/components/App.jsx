23f6f5c93a5cbd4370b6c62eb1f834a48a1f9f7e
Minty <82933018+kjkiwi@users.noreply.github.com>
2022-05-30 11:21:51 +1200

    input value pushed to <h1> on click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("You");
+  const [click, setClick] = useState(false);
+
+  const fullName = [];
+
+  function typeName(event) {
+    fullName.push(event.target.value);
+  }
+
+  function mouseEvent(event) {
+    setClick(true);
+    setName(fullName.pop());
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {name} </h1>
+      <input onChange={typeName} type="text" placeholder="What's your name?" />
+      <button
+        style={{ backgroundColor: click ? "black" : "white" }}
+        onClick={mouseEvent}
+      >
+        Submit
+      </button>
     </div>
   );
 }
 
 export default App;
+
+//CODE TO CHANGE BUTTON COLOR ON MOUSE CLICK
+
+// function App() {
+
+//   const [name, setName] = useState("You")
+//   const [click, setClick] = useState(false)
+
+//   function typeName(event) {
+//     setName(event.target.value)
+//   }
+
+//   function mouseEvent(event) {
+// setClick (true)
+//   }
+
+//   return (
+//     <div className="container">
+//       <h1>Hello {name} </h1>
+//       <input onChange={typeName} type="text" placeholder="What's your name?" />
+//       <button style={{backgroundColor: click ? 'black': 'white'}} onClick={mouseEvent} >Submit</button>
+//     </div>
+//   );
+// }
